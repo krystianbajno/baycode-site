72d661f82c59cfda678d1387e73710f9f268ea7b
krystian <krystian.bajno@liteweb.io>
2021-06-06 05:16:54 +0200

    Minor updates

--- src/components/organisms/blog/BlogSearch.tsx
+++ src/components/organisms/blog/BlogSearch.tsx
@@ -2,10 +2,13 @@ import * as React from "react"
 
 import { debounce }  from 'simple-debouncer'
 import FormInput from "../../molecules/common/FormInput"
+import "../../../assets/styles/components/organisms/articles/blog-search.scss";
 
 export default (props) => {
-  return <FormInput
-    onChange={(change) => debounce(() => props.onSearch(change), 500)}
-    placeholder="Search articles"
-  />
+  return <div className="blog-search">
+    <FormInput
+      onChange={(change) => debounce(() => props.onSearch(change), 500)}
+      placeholder="Search articles"
+    />
+  </div>
 }
